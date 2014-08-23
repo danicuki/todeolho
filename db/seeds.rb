9c759557627819909f2869d7f75c2a0fa7dfef08
Duke <duke@riseup.net>
2014-08-23 17:14:49 -0300

    starting Notification seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+PID = 23
+
+Notification.create! politician_id: PID, category: "Processo"
+Notification.create! politician_id: PID, category: "Processo"
+Notification.create! politician_id: PID, category: "Patrimônio"
+Notification.create! politician_id: PID, category: "Processo"
+Notification.create! politician_id: PID, category: "Processo"
+Notification.create! politician_id: PID, category: "Projeto"
+Notification.create! politician_id: PID, category: "Processo"
