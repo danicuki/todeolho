c2be18a6630cbcab67d6a2467377f2fc16f94065
Duke <duke@riseup.net>
2014-08-23 16:09:51 -0300

    removed "O que é?" starting autocomplete

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -2,4 +2,9 @@ class PoliticiansController < ApplicationController
   def show
     @politician = Politician.find(params[:id])
   end
+
+  def index
+    @politicians = Politician.all
+    render json: @politicians
+  end
 end
