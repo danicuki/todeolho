468aea74d97f639288311dab410e42eae64c8ba4
Daniel Cukier <danicuki@gmail.com>
2014-08-23 18:01:03 -0300

    redirect message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Agora você receberá as novidades desse candidato' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
