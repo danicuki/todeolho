012b8c04993ae33e60564a1612b5e40bf70e2e23
Raul Souza Lima <raulsouzalima@gmail.com>
2014-08-26 00:34:38 -0300

    Removed duplicated document.ready on application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,33 +5,30 @@
 //= require select2
 //= require_tree .
 //
-$(document).ready(function(){
-  $(function() {
-    $("#searchPolitician").select2({
-      placeholder: "Busque por um politico",
-      minimumInputLength: 1,
-      ajax: {
-        url: "/politicians.json",
-        dataType: 'json',
-        data: function (term, page) {
-          return {
-            q: term,
-            limit: 10
-          };
-        },
-        results: function (data, page) {
-          return {results: data};
-        }
+$(function() {
+  $("#searchPolitician").select2({
+    placeholder: "Busque por um politico",
+    minimumInputLength: 1,
+    ajax: {
+      url: "/politicians.json",
+      dataType: 'json',
+      data: function (term, page) {
+        return {
+          q: term,
+          limit: 10
+        };
       },
-      formatResult: function(p){
-        if(p.image)  return "<div class='img-mask'> <img src='" + p.image  + "'/></div><span class='name'>"+ p.nickname+"</span>";
-        return p.name;
-      },
-      dropdownCssClass: "bigdrop",
-      escapeMarkup: function (m) { return m; }
-    }).on("change", function(e){
-      window.location = "/politicians/"+e.val;
-    });
+      results: function (data, page) {
+        return {results: data};
+      }
+    },
+    formatResult: function(p){
+      if(p.image)  return "<div class='img-mask'> <img src='" + p.image  + "'/></div><span class='name'>"+ p.nickname+"</span>";
+      return p.name;
+    },
+    dropdownCssClass: "bigdrop",
+    escapeMarkup: function (m) { return m; }
+  }).on("change", function(e){
+    window.location = "/politicians/"+e.val;
   });
-
 });
