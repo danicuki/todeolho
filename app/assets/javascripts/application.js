35592739d99ef8bd62a013c234c2d88bd968029d
Érica Hidemi Mitsuishi <mitsuishihidemi@MacBook-Air-de-Erica.local>
2014-08-27 23:45:43 -0300

    corrigindo tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,30 +6,49 @@
 //= require select2_locale_pt-BR
 //= require_tree .
 //
+
 $(function() {
+  
   $("#searchPolitician").select2({
-      placeholder: "Busque por um politico",
-      ajax: {
-        url: "/politicians.json",
-        dataType: 'json',
-        data: function (term, page) {
-          return {
-            q: term,
-            limit: 10
-          };
-        },
-        results: function (data, page) {
-          return {results: data};
-        }
+    placeholder: "Busque por um politico",
+    ajax: {
+      url: "/politicians.json",
+      dataType: 'json',
+      data: function (term, page) {
+        return {
+          q: term,
+          limit: 10
+        };
       },
-      formatResult: function(p){
-        if(p.image)  return "<div class='img-mask'> <img src='" + p.image  + "'/></div><span class='name'>"+ p.nickname+"</span>";
-        return p.name;
-      },
-      minimumInputLength: 3,
-      dropdownCssClass: "bigdrop",
-      escapeMarkup: function (m) { return m; }
-    }).on("change", function(e){
-      window.location = "/politicians/"+e.added.uri;
-    });
+      results: function (data, page) {
+        return {results: data};
+      }
+    },
+    formatResult: function(p){
+      if(p.image)  return "<div class='img-mask'> <img src='" + p.image  + "'/></div><span class='name'>"+ p.nickname+"</span>";
+      return p.name;
+    },
+    minimumInputLength: 3,
+    dropdownCssClass: "bigdrop",
+    escapeMarkup: function (m) { return m; }
+  }).on("change", function(e){
+    window.location = "/politicians/"+e.added.uri;
+  });
+
+  $('#notifications-button').click(function() {
+    $('.active').removeClass();
+    $(this).closest('li').addClass('active');
+    $('#infos-section').hide();
+    $('#notifications-section').show();
+    return false;
+  });
+
+  $('#infos-button').click(function() {
+    $('.active').removeClass();
+    $(this).closest('li').addClass('active');
+    $('#notifications-section').hide();
+    $('#infos-section').show();
+    return false;
+  });
+
 });
